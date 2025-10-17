557b0f9449eadb7dc4461fdd21991630dc60ed00
Abdullah Yazji <abdullah1yazji@gmail.com>
2025-10-17 16:12:00 +0300

    Arrays

--- main.go
+++ main.go
@@ -1,17 +1,29 @@
 package main
 
-import (
-	"firstProgram/secondPackage"
-	"fmt"
-)
+import "fmt"
+
+// import (
+// 	"firstProgram/secondPackage"
+// 	"fmt"
+// )
 
 func main() {
 	// print("hello")
-	const name = "Abdullah"
-	var age int = 20
-	fmt.Println("Hello", name)
-	fmt.Println("age:", age)
-	SecondFile()
-	secondPackage.SecondPackageFun()
-	// secondPackage.secondPackageFun() // private --> error: undefined
+	// const name = "Abdullah"
+	// var age int = 20
+	// fmt.Println("Hello", name)
+	// fmt.Println("age:", age)
+	// SecondFile()
+	// secondPackage.SecondPackageFun()
+	// // secondPackage.secondPackageFun() // private --> error: undefined
+
+	// var studentsNames = [4]string{"Ahmed", "Ali, Abdullah, Mohammed"}
+	studentsGrades := [5]int{10, 20, 40, 50, 30}
+	// studentsGrades[2] = 100
+
+	// fmt.Println(studentsNames[0])
+	// fmt.Println(studentsNames[1:4])
+
+	fmt.Println(studentsGrades)
+
 }
