20d4bcad781df8de6be9bf791a02b000b2315816
Abdullah Yazji <abdullah1yazji@gmail.com>
2025-10-22 11:00:02 +0300

    Slices

--- main.go
+++ main.go
@@ -17,13 +17,27 @@ func main() {
 	// secondPackage.SecondPackageFun()
 	// // secondPackage.secondPackageFun() // private --> error: undefined
 
-	// var studentsNames = [4]string{"Ahmed", "Ali, Abdullah, Mohammed"}
-	studentsGrades := [5]int{10, 20, 40, 50, 30}
-	// studentsGrades[2] = 100
+	// // var studentsNames = [4]string{"Ahmed", "Ali, Abdullah, Mohammed"}
+	// studentsGrades := [5]int{10, 20, 40, 50, 30}
+	// // studentsGrades[2] = 100
 
-	// fmt.Println(studentsNames[0])
-	// fmt.Println(studentsNames[1:4])
+	// // fmt.Println(studentsNames[0])
+	// // fmt.Println(studentsNames[1:4])
 
-	fmt.Println(studentsGrades)
+	// fmt.Println(studentsGrades)
+
+	// Slices
+	var numbers []int
+	names := []string{"Ahmed", "Abdullah", "Ali"}
+	namesLength := len(names)
+	names = append(names, "Mohammed")
+	namesLength1 := len(names)
+	fmt.Println(numbers)
+	fmt.Println(names)
+	fmt.Println(namesLength)
+	fmt.Println(namesLength1)
+	namesCopy := make([]string, len(names))
+	copy(namesCopy, names)
+	fmt.Println("Copy:", namesCopy)
 
 }
