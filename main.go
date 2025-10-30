4af9f76474b28f2b98e015a8f161f5f1b695488b
Abdullah Yazji <abdullah1yazji@gmail.com>
2025-10-30 11:26:50 +0300

    Variadic Functions

--- main.go
+++ main.go
@@ -11,17 +11,27 @@ import "fmt"
 // 	"fmt"
 // )
 
-func plus(a int, b int) int {
+// func plus(a int, b int) int {
 
-	return a + b
-}
+// 	return a + b
+// }
 
-func plusPlus(a, b, c int) int {
-	return a + b + c
-}
+// func plusPlus(a, b, c int) int {
+// 	return a + b + c
+// }
 
-func vals() (int, int) {
-	return 3, 8
+// func vals() (int, int) {
+// 	return 3, 8
+// }
+
+func sum(nums ...int) {
+	fmt.Print(nums, " ")
+	total := 0
+
+	for _, num := range nums {
+		total += num
+	}
+	fmt.Println((total))
 }
 
 func main() {
@@ -179,17 +189,22 @@ func main() {
 	// 	fmt.Println("n === n2")
 	// }
 
-	res := plus(1, 2)
-	fmt.Println("1+2 =", res)
+	// res := plus(1, 2)
+	// fmt.Println("1+2 =", res)
+	// res = plusPlus(1, 2, 3)
+	// fmt.Println("1+2+3 =", res)
 
-	res = plusPlus(1, 2, 3)
-	fmt.Println("1+2+3 =", res)
+	// a, b := vals()
+	// fmt.Println(a)
+	// fmt.Println(b)
 
-	a, b := vals()
-	fmt.Println(a)
-	fmt.Println(b)
+	// _, c := vals()
+	// fmt.Println(c)
 
-	_, c := vals()
-	fmt.Println(c)
+	sum(1, 2)
+	sum(1, 2, 3)
+
+	nums := []int{1, 2, 3, 4}
+	sum(nums...)
 
 }
